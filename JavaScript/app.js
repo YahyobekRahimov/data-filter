3b129706ce8a33bae6bcc1090c23388af704bc34
YahyobekRahimov <rcoder570@gmail.com>
2023-11-16 16:38:07 +0500

    reornagized code

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,63 +1,33 @@
 import { data as carsData} from "./data.js";
 const tbody = document.querySelector('tbody');
 
-let data = carsData;
+let data = [...carsData];
 let colorSelect = document.getElementById('color');
 
+const searchInput = document.querySelector('input[name="search"]');
+const select = document.getElementById('carName');
+const carNames = [];
+const carColors = [];
+const carYearStart = document.getElementById('carYearStart');
+const priceStart = document.getElementById('carPriceStart');
+const priceEnd = document.getElementById('carPriceEnd');
+const statusSelect = document.getElementById('carStatus');
 data.forEach(element => {
     element.status = element.status == 'active' ? "Sotuvda" : 'Sotuvda emas' 
 })
 
 displayDataOnTable(data);
 
-function displayDataOnTable(dataSet) {
-    let dataToBeDisplayed = ``;
-    dataSet.forEach((item, index) => {
-       const carData = `
-       <tr>
-            <td>${index + 1}</td>
-            <td>${item.name}</td>
-            <td>${item.year}</td>
-            <td>${item.color}</td>
-            <td>$${item.price.toLocaleString()}</td>
-            <td>${item.status}</td>
-        </tr>`;
-       dataToBeDisplayed += carData; 
-    })
-    tbody.innerHTML = dataToBeDisplayed;
-}
 
 
-const searchInput = document.querySelector('input[name="search"]');
+
 
 searchInput.addEventListener('input', function() {
     displayFilterResults(searchInput.value)
 })
 
-function displayFilterResults(input) {
-    const searchLength = input.trim().length;
-    let dataToBeDisplayed = '';
-    data.forEach((element, index) => {
-        if (element.name.slice(0, searchLength).toLocaleLowerCase() == input.trim().toLocaleLowerCase()) {
-            const carData = `
-            <tr>
-                <td>${index + 1}</td>
-                <td>${element.name}</td>
-                <td>${element.year}</td>
-                <td>${element.color}</td>
-                <td>$${element.price.toLocaleString()}</td>
-                <td>${element.status}</td>
-            </tr>`
-            dataToBeDisplayed += carData;
-        }
-    })
-    tbody.innerHTML = dataToBeDisplayed;
-}
 
-const select = document.getElementById('carName');
 
-const carNames = [];
-const carColors = [];
 
 data.forEach(element => {
     carNames.push(element.name);
@@ -67,22 +37,13 @@ data.forEach(element => {
 handleOptionsInSelect();
 handleOptionsForColorSelect();
 
-function handleOptionsInSelect() {
-    const optionsAlreadyEntered = [];
-    for (let i = 0; i < carNames.length; i++) {
-        const element = carNames[i];
-        if (optionsAlreadyEntered.includes(element)) {
-            continue;
-        };
-        optionsAlreadyEntered.push(element);
-        displayOption(element);
-    }
-}
 
-function displayOption(carName) {
-    let str = `<option value="${carName}">${carName}</option>`
-    select.innerHTML += str;
-}
+// function filterByName(data, target) {
+//     const newDataSet = data.filter(element => {
+//         return element ==
+//     })
+// }
+
 
 select.addEventListener('change', function() {
     data = carsData;
@@ -97,8 +58,8 @@ select.addEventListener('change', function() {
     data = newDataSet;
 })
 
-const carYearStart = document.getElementById('carYearStart');
 carYearStart.addEventListener('input', function() {
+    if (!data.length) return;
     const newDataSet = data.filter(element => {
         return carYearStart.value <= element.year;
     });
@@ -120,6 +81,34 @@ colorSelect.addEventListener('change', function() {
     data = newDataSet;
 })
 
+
+priceStart.addEventListener('input', function() {
+    const newDataSet = data.filter((element, index) => {
+        return element.price >= parseInt(priceStart.value) && element.price <= parseInt(priceEnd.value);
+    })
+    displayDataOnTable(newDataSet);
+})
+priceEnd.addEventListener('input', function() {
+    const newDataSet = data.filter((element, index) => {
+        return element.price >= parseInt(priceStart.value) && element.price <= parseInt(priceEnd.value);
+    })
+    displayDataOnTable(newDataSet);
+})
+
+
+statusSelect.addEventListener('change', function() {
+    const newDataSet = [];
+    data.forEach(element => {
+        console.log(element.status + statusSelect.value);
+        if (element.status == statusSelect.value) {
+            newDataSet.push(element);
+        }
+    })
+    displayDataOnTable(newDataSet);
+})
+
+// ! Functions 
+
 function handleOptionsForColorSelect() {
     const optionsAlreadyEntered = [];
     for (let i = 0; i < carColors.length; i++) {
@@ -137,31 +126,56 @@ function displayOptionInColorSelect(carColor) {
     colorSelect.innerHTML += str;
 }
 
-const priceStart = document.getElementById('carPriceStart');
-const priceEnd = document.getElementById('carPriceEnd');
+function handleOptionsInSelect() {
+    const optionsAlreadyEntered = [];
+    for (let i = 0; i < carNames.length; i++) {
+        const element = carNames[i];
+        if (optionsAlreadyEntered.includes(element)) {
+            continue;
+        };
+        optionsAlreadyEntered.push(element);
+        displayOption(element);
+    }
+}
 
-priceStart.addEventListener('input', function() {
-    const newDataSet = data.filter((element, index) => {
-        return element.price >= parseInt(priceStart.value) && element.price <= parseInt(priceEnd.value);
-    })
-    displayDataOnTable(newDataSet);
-})
-priceEnd.addEventListener('input', function() {
-    const newDataSet = data.filter((element, index) => {
-        return element.price >= parseInt(priceStart.value) && element.price <= parseInt(priceEnd.value);
-    })
-    displayDataOnTable(newDataSet);
-})
+function displayOption(carName) {
+    let str = `<option value="${carName}">${carName}</option>`
+    select.innerHTML += str;
+}
 
-const statusSelect = document.getElementById('carStatus');
-
-statusSelect.addEventListener('change', function() {
-    const newDataSet = [];
-    data.forEach(element => {
-        console.log(element.status + statusSelect.value);
-        if (element.status == statusSelect.value) {
-            newDataSet.push(element);
+function displayFilterResults(input) {
+    const searchLength = input.trim().length;
+    let dataToBeDisplayed = '';
+    data.forEach((element, index) => {
+        if (element.name.slice(0, searchLength).toLocaleLowerCase() == input.trim().toLocaleLowerCase()) {
+            const carData = `
+            <tr>
+                <td>${index + 1}</td>
+                <td>${element.name}</td>
+                <td>${element.year}</td>
+                <td>${element.color}</td>
+                <td>$${element.price.toLocaleString()}</td>
+                <td>${element.status}</td>
+            </tr>`
+            dataToBeDisplayed += carData;
         }
     })
-    displayDataOnTable(newDataSet);
-})
+    tbody.innerHTML = dataToBeDisplayed;
+}
+
+function displayDataOnTable(dataSet) {
+    let dataToBeDisplayed = ``;
+    dataSet.forEach((item, index) => {
+       const carData = `
+       <tr>
+            <td>${index + 1}</td>
+            <td>${item.name}</td>
+            <td>${item.year}</td>
+            <td>${item.color}</td>
+            <td>$${item.price.toLocaleString()}</td>
+            <td>${item.status}</td>
+        </tr>`;
+       dataToBeDisplayed += carData; 
+    })
+    tbody.innerHTML = dataToBeDisplayed;
+}
